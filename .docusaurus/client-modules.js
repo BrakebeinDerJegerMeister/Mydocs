1d2ad5370a7d9719f53ccfa055bc156443e55f90
BrakebeinDerJegerMeister <leperspikasse@gmail.com>
2024-02-19 16:16:18 +0100

    1er commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/brakebein/Projets/mydocs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/brakebein/Projets/mydocs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/brakebein/Projets/mydocs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/brakebein/Projets/mydocs/src/css/custom.css"),
+];
